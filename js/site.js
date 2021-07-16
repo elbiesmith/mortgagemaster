61dd17d7fb0eb019e5f0b73dd13b441a57f7d8c4
elbiesmith <elbie.smith@gmail.com>
2021-07-16 18:51:08 -0500

    initial

---
+++ js/site.js
@@ -0,0 +1,10 @@
+// Total monthly payment (loanAmount) * (loanRate/1200)/(1-(1+loanRate/1200)(-loanTerm)
+// Remaining Balance BEFORE the first month it equals the amount of the loan
+// Interest Payment = Previous remaining balance * loanRate/1200
+// Prinicpal Payment = Total Monthly payment - interest payment
+// Remaining Balance AFTER each month, Previous Remaining Balance - principal payments.
+
+function calculate() {
+
+}
+
